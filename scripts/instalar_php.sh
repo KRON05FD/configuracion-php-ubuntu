63722199d262e167366d893c4f711e48c40ecaa5
KRON05FD <105858747+KRON05FD@users.noreply.github.com>
2025-08-14 05:53:06 -0500

    Agrego extenciones nuevas a la instalacion php

--- scripts/instalar_php.sh
+++ scripts/instalar_php.sh
@@ -7,5 +7,8 @@ sudo apt update -y
 echo "Instalando Apache y PHP..."
 sudo apt install apache2 php libapache2-mod-php php-mysql -y
 
+echo "Agregar extenciones"
+sudo apt install php-xml php-gd php-curl -y
+
 echo "Verificando versión de PHP..."
 php -v
